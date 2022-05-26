db6bd9ba47be9aa68e38fb63c881a56fc644f065
domico <103330935+domico@users.noreply.github.com>
2022-05-26 09:05:14 -0400

    Add files via upload

--- esercizio1.sh
+++ esercizio1.sh
@@ -2,6 +2,26 @@
 
 #----------------- Functions ----------------------
 
+ch_root_perm() {
+	#check root permission 
+	if [ "$EUID" -ne 0 ]
+	  then echo "Please run as root"
+	  exit
+	fi
+}
+
+
+
+check_path() {
+	#path validation
+	if ! [ -d "${path}" ]; then
+		echo 'ERROR: Path does not exist'
+		exit 0
+	else
+		firstPath=${path} #origina path used to avoid navigation on parents
+	fi	
+}
+
 show_home_menu() {
 	#function used to show the main menu
 	echo "--- Current path: ${path} ---"
@@ -11,6 +31,8 @@ show_home_menu() {
 	echo "Select one action:"
 	echo "1. Navigate inside directories"
 	echo "2. Edit files and directories permissions"
+	echo "3. Edit User Owner"
+	echo "4. Edit Group Owner"
 	echo "q. Quit"
 	echo -en "> "
 		
@@ -28,6 +50,10 @@ do_home_menu() {
 				show_navigate_menu;;
 			"2")
 				edit_permission;;
+			"3")
+				edit_user_owner;;
+			"4")
+				edit_group_owner;;
 			"q")
 				exit 0;;
 			*)
@@ -81,9 +107,14 @@ show_navigate_menu(){
 }
 
 edit_permission(){
+	
+	epdecision=-1
+	while [ $epdecision != "q" ]	
+	
+
 	echo " "
-	echo "--- Current path: ${path} ---"
-	echo "$(ls -la)"
+	echo "---- Current path: ${path} ----"
+	echo "$(ls -la "${path}")"
 	echo " "
 	echo "Select file or directory you want to change permissions to:"
 
@@ -101,10 +132,8 @@ edit_permission(){
 
 	echo "q. Quit"
 	echo " "
-	echo -en "> "	
-
-	epdecision=-1
-	while [ $epdecision != "q" ]
+	echo -en "> "
+	
 	do
 		read epdecision
 		if [ $epdecision = "q" ]; then
@@ -121,11 +150,179 @@ edit_permission(){
 
 change_perm() {	
 	chmod_path="${path}"/"$1"
-	chmod a+x "${chmod_path}fef"
-	chmod_error=$?
+	chmod_command=-1
+	while [ $chmod_command != "q" ]; do
+		echo " "
+		echo " --- Current File/Directory Involved ---"
+		echo " --- "${chmod_path}" --- "	
+		echo " --- $(ls -l "$chmod_path" )"
+		echo " "
+		echo "Insert the chmod permission you want to use or press q to Quit:"
+		echo -en "> "	
+	
+		read chmod_command
+
+		chmod_out=$( chmod "${chmod_command}" "${chmod_path}" 2>&1 )
+		chmod_error=$?
+
+		if [ $chmod_error = 0 ]; then
+			#echo "Nessun errore"
+			return 0
+		else
+			#echo "Errore"
+			echo "$chmod_out"
+		fi
+	done
+
+	return 0
 }
 
+edit_user_owner(){
+	
+	epdecision=-1
+	while [ $epdecision != "q" ]	
+	
+
+	echo " "
+	echo "--- Current path: ${path} ---"
+	echo "$(ls -la ${path})"
+	echo " "
+	echo "Select file or directory you want to change user owner to:"
+
+	content=("$(ls -a ${path})") #storing content of current dir
+	content_to_hide=(". ..") #content to hide of current dir
+	for elem in ${content_to_hide}
+	do
+		content=(${content[@]/${elem}})
+	done
+
+	for index in $(seq 0 $((${#content[@]} - 1)) )
+	do
+		echo "${index}. ${content[${index}]} "
+	done
+
+	echo "q. Quit"
+	echo " "
+	echo -en "> "
+	
+	do
+		read epdecision
+		if [ $epdecision = "q" ]; then
+			return 0
+		elif [[ $epdecision =~ ^-?[0-9]+$ ]] && [ $epdecision -ge 0 ] && [ $epdecision -le $((${#content[@]} -1)) ] 
+		then
+			#echo "${epdecision}"
+			change_user_owner "${content[$epdecision]}"
+		else
+			echo "Invalid Input"
+		fi		
+	done	
+}
+edit_group_owner(){
+	
+	epdecision=-1
+	while [ $epdecision != "q" ]	
+	
+
+	echo " "
+	echo "--- Current path: ${path} ---"
+	echo "$(ls -la ${path})"
+	echo " "
+	echo "Select file or directory you want to change user owner to:"
+
+	content=("$(ls -a ${path})") #storing content of current dir
+	content_to_hide=(". ..") #content to hide of current dir
+	for elem in ${content_to_hide}
+	do
+		content=(${content[@]/${elem}})
+	done
+
+	for index in $(seq 0 $((${#content[@]} - 1)) )
+	do
+		echo "${index}. ${content[${index}]} "
+	done
+
+	echo "q. Quit"
+	echo " "
+	echo -en "> "
+	
+	do
+		read epdecision
+		if [ $epdecision = "q" ]; then
+			return 0
+		elif [[ $epdecision =~ ^-?[0-9]+$ ]] && [ $epdecision -ge 0 ] && [ $epdecision -le $((${#content[@]} -1)) ] 
+		then
+			#echo "${epdecision}"
+			change_group_owner "${content[$epdecision]}"
+		else
+			echo "Invalid Input"
+		fi		
+	done	
+}
+
+change_user_owner() {	
+	us_ow_path="${path}"/"$1"
+	us_ow_command=-1
+	while [ $us_ow_command != "q" ]; do
+		echo " "
+		echo " --- Current File/Directory Involved ---"
+		echo " --- "${us_ow_path}" --- "	
+		echo " --- $(ls -l "$us_ow_path" )"
+		echo " "
+		echo "Insert the new user owner or press q to Quit:"
+		echo -en "> "	
+	
+		read us_ow_command
+
+		us_ow_out=$( chown "${us_ow_command}" "${us_ow_path}" 2>&1 )
+		us_ow_error=$?
+
+		if [ $us_ow_error = 0 ]; then
+			#echo "Nessun errore"
+			return 0
+		else
+			#echo "Errore"
+			echo "$us_ow_out"
+		fi
+	done
+
+	return 0
+}
+
+change_group_owner() {	
+	gr_ow_path="${path}"/"$1"
+	gr_ow_command=-1
+	while [ $gr_ow_command != "q" ]; do
+		echo " "
+		echo " --- Current File/Directory Involved ---"
+		echo " --- "${gr_ow_path}" --- "	
+		echo " --- $(ls -l "$gr_ow_path" )"
+		echo " "
+		echo "Insert the new group owner or press q to Quit:"
+		echo -en "> "	
+	
+		read gr_ow_command
+
+		gr_ow_out=$( chown :"${gr_ow_command}" "${gr_ow_path}" 2>&1 )
+		gr_ow_error=$?
+
+		if [ $gr_ow_error = 0 ]; then
+			#echo "Nessun errore"
+			return 0
+		else
+			#echo "Errore"
+			echo "$gr_ow_out"
+		fi
+	done
+
+	return 0
+}
+
+
+
 #----------------- End Funcions -------------------
+
+#--------------------- Main -----------------------
 
 #checking for path option
 while getopts p: flag
@@ -135,14 +332,10 @@ do
 	esac
 done
 
-#path validation
-if ! [ -d "${path}" ]; then
-	echo 'ERROR: Path does not exist'
-	exit 0
-else
-	firstPath=${path} #origina path used to avoid navigation on parents
-fi	
+ch_root_perm
 
-#run menu
+check_path
+
 do_home_menu
+
 
